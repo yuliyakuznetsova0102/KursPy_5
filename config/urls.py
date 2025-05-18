ad4c7d11a4344c85cf290959293c6bde5ed4cbd9
Yuliya Kuznetsova <juliakuznetsova001@gmail.com>
2025-05-18 18:28:47 +0300

    итог работа

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib import admin
 
@@ -42,5 +41,3 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
-
-
